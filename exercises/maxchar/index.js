637989e296e0646438f15565a033891c7b3220f4
Eric Rodgers <ericerodgers@yahoo.com>
2024-08-21 15:05:40 -0400

    works

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const obj = {};
+    for (const char of str) {
+        if (obj[char]) {
+            obj[char] += 1
+        } else [
+            obj[char] = 1
+        ]
+    }
+    let max = 0;
+    let maxChar = '';
+    for (const char in obj) {
+        if (obj[char] > max) {
+            max = obj[char];
+            maxChar = char;
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
