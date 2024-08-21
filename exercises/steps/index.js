55c9ea736d04cbe1d7f1d8a932e49aa8c7bb6191
Eric Rodgers <ericerodgers@yahoo.com>
2024-08-21 17:42:34 -0400

    works

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,37 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// RECURSIVE (NOT NECESSARILY BETTER THAN ITERATIVE...):
+function steps(n, row = 0, step = '') {
+    if (row === n) {
+        return
+    }
+    if (step.length === n) {
+        console.log(step)
+        return steps(n, row + 1)
+    }
+    if (step.length <= row) {
+        step += '#'
+    } else {
+        step += ' '
+    }
+
+    steps(n, row, step)
+}
 
 module.exports = steps;
+
+// ITERATIVE:
+// function steps(n) {
+//     for (let row = 1; row <=n; row++) {
+//         let step = ''
+//         for (let col = 1; col <= n; col++) {
+//             if (col <= row) {
+//                 step += "#"
+//             } else {
+//                 step += ' '
+//             }
+//         }
+//         console.log(step)
+//     }
+// }
