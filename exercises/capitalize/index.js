0ba9f28c5b84058a4db56a3ed04c8db01d63adc3
Eric Rodgers <ericerodgers@yahoo.com>
2024-08-21 16:51:07 -0400

    works

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,25 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const arr = [];
+    let i = 0
+    while (i < str.length) {
+        if (i === 0 || str[i - 1] === " ") {
+            arr.push(str[i].toUpperCase())
+        } else {
+            arr.push(str[i])
+        }
+        i += 1;
+    }
+    return arr.join("")
+
+    // const output = []
+    // const arr = str.split(' ')
+    // for (const word of arr) {
+    //     output.push(word[0].toUpperCase() + word.slice(1))
+    // }
+    // return output.join(' ')
+}
 
 module.exports = capitalize;
