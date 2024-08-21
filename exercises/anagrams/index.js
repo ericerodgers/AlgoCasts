a2c94ea8b11cf944bc6e85ed9247e1484ce9ce70
Eric Rodgers <ericerodgers@yahoo.com>
2024-08-21 16:30:21 -0400

    works

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,41 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const cleanA = cleanStr(stringA)
+    const cleanB = cleanStr(stringB)
+    return cleanA === cleanB;
+
+
+    // const cleanA = stringA.replace(/[\s\p{P}]/gu, '').toLowerCase();
+    // const cleanB = stringB.replace(/[\s\p{P}]/gu, '').toLowerCase();
+    // if (cleanA.length !== cleanB.length) return false;
+    // const newA = cleanA.split("").sort().join("")
+    // const newB = cleanB.split("").sort().join("")
+    // if (newA !== newB) return false
+    // return true;
+
+
+
+    // const cleanA = stringA.replace(/[\s\p{P}]/gu, '').toLowerCase();
+    // const cleanB = stringB.replace(/[\s\p{P}]/gu, '').toLowerCase();
+    // if (cleanA.length !== cleanB.length) return false;
+    // const charObjA = {};
+    // const charObjB = {};
+    // for (let i = 0; i < cleanA.length; i++) {
+    //     charObjA[cleanA[i]] = charObjA[cleanA[i]] + 1 || 1;
+    //     charObjB[cleanB[i]] = charObjB[cleanB[i]] + 1 || 1;
+    // }
+    // for (const [key, val] of Object.entries(charObjA)) {
+    //     if (charObjB[key] !== val) {
+    //         return false
+    //     }
+    // }
+    // return true;
+}
+
+const cleanStr = (str) => {
+    return str.replace(/[\s\p{P}]/gu, '').toLowerCase().split("").sort().join("")
+}
 
 module.exports = anagrams;
