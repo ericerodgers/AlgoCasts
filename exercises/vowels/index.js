de59faaa6565a0e30347e4799bdc07ceb776675d
Eric Rodgers <ericerodgers@yahoo.com>
2024-08-21 22:33:03 -0400

    works

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    // USING REGEX:
+    const cleanStr = str.toLowerCase();
+    const vowelsArr = cleanStr.match(/[aeiou]/gi)
+    return vowelsArr ? vowelsArr.length : 0;
+
+    // ITERATIVE:
+    // const cleanStr = str.toLowerCase();
+    // const vowels = {"a": true, "e": true, "i": true, "o": true, "u": true}
+    // let numVowels = 0
+    // for (const char of cleanStr) {
+    //     if (vowels[char]) {
+    //         numVowels += 1
+    //     }
+    // }
+    // return numVowels
+}
 
 module.exports = vowels;
