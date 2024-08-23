2e8e33bcc21a49f8e6a8fb59bfc1173d00562682
Eric Rodgers <ericerodgers@yahoo.com>
2024-08-23 11:47:44 -0400

    works

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const counters = [0]
+    const arr = [root, "stop"]
+    while (arr.length > 1) {
+        const node = arr.shift()
+        if (node === "stop") {
+            counters.push(0) // starts a new row's width counter
+            arr.push("stop") // adds our stopper to the end of the array again
+        } else {
+            arr.push(...node.children)
+            counters[counters.length - 1] += 1 // increments counter for the level we are on
+        }
+    }
+    return counters
+}
 
 module.exports = levelWidth;
